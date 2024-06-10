1acd317a146ea7080bcaf1bc37dc96a3e82641d5
Jules Broto <77014034+JuleSpace@users.noreply.github.com>
2024-06-10 11:52:16 +0200

    Revert "updates"
    This reverts commit 2c18c0bfbdf79add303a914f090eb3b5c2fdce4d.

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -3,11 +3,9 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity]
-class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
+class Utilisateur
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -26,8 +24,7 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string', length: 100)]
     private $password;
 
-    #[ORM\Column(type: 'json')]
-    private $roles = [];
+    // Getters and Setters
 
     public function getId(): ?int
     {
@@ -70,7 +67,7 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function getPassword(): string
+    public function getPassword(): ?string
     {
         return $this->password;
     }
@@ -81,43 +78,5 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
-
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
-    }
-
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    public function getSalt(): ?string
-    {
-        // not needed for bcrypt or sodium
-        return null;
-    }
-
-    public function getUsername(): string
-    {
-        return $this->email;
-    }
-
-    public function getUserIdentifier(): string
-    {
-        return $this->email;
-    }
-
-    public function eraseCredentials(): void
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-    }
 }
-
 ?>
