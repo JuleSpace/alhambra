a4bd708beb3178db32b61b35fded6a0f1cc9a7f5
Jules Broto <77014034+JuleSpace@users.noreply.github.com>
2024-07-10 14:25:20 +0200

    index

---
+++ frontend/src/views/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Bienvenue sur l'application de gestion de l'association Alhambra</h1>
+  
+      <ul>
+        <li>Gestion des Utilisateurs</li>
+        <li>Gestion des Commissions</li>
+        <li>Gestion des Posts</li>
+        <li>Gestion des Privilèges</li>
+        <li>Accès au chat</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home',
+    metaInfo: {
+      title: 'Accueil'
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Ajoutez vos styles ici */
+  </style>
+  
