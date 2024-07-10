fc0bba551e27d4d3d122bdb73c6d3f7e4b6b1ca2
Jules Broto <77014034+JuleSpace@users.noreply.github.com>
2024-07-10 15:40:52 +0200

    update

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,27 +1,26 @@
 <template>
-    <div>
-      <h1>Bienvenue sur l'application de gestion de l'association Alhambra</h1>
-  
-      <ul>
-        <li>Gestion des Utilisateurs</li>
-        <li>Gestion des Commissions</li>
-        <li>Gestion des Posts</li>
-        <li>Gestion des Privilèges</li>
-        <li>Accès au chat</li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Home',
-    metaInfo: {
-      title: 'Accueil'
-    }
+  <div>
+    <h1>Bienvenue sur l'application de gestion de l'association Alhambra</h1>
+
+    <ul>
+      <li>Gestion des Utilisateurs</li>
+      <li>Gestion des Commissions</li>
+      <li>Gestion des Posts</li>
+      <li>Gestion des Privilèges</li>
+      <li>Accès au chat</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  metaInfo: {
+    title: 'Accueil'
   }
-  </script>
-  
-  <style scoped>
-  /* Ajoutez vos styles ici */
-  </style>
-  
+}
+</script>
+
+<style scoped>
+/* Ajoutez vos styles ici */
+</style>
